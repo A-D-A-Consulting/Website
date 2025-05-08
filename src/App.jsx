84d84938f3f24d3829d0fc86cfc992a09594c1a7
zqalon <89801024+zqalon@users.noreply.github.com>
2025-05-08 11:27:38 -0700

    start

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import "./App.css";
 function App() {
   return (
     <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="/ADA.jpg" alt="ADA Logo" className="w-64 h-auto mx-auto" />
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
+      <h1 className="text-3xl text-center font-bold underline">A.D.A Consulting Website</h1>
+      <p className="text-center text-xl">This is the official website for A.D.A. Consulting.</p>
+      <img src="/ADA.jpg" alt="ADA Logo" className="w-100 h-auto mx-auto" />
     </main>
   );
 }
