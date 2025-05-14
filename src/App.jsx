06469e6ad2aa04246b962d0cda7558f5a59321d7
zqalon <89801024+zqalon@users.noreply.github.com>
2025-05-14 12:07:40 -0700

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
 import "./App.css";
-import Button from "./components/Button";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Industries from "./pages/Industries.jsx";
 import Contact from "./pages/Contact.jsx";
 import Careers from "./pages/Careers.jsx";
 
+
+
 function App() {
+  const navigate = useNavigate();
+
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
+    <main>
+      <nav className="bg-black text-white p-4 shacdow-md">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="text-xl font-bold">
+            <button onClick={() => navigate('/')}>A.D.A. Consulting</button>
+          </div>
+        </div>
+      </nav>
 
     <img src="/ADA.jpg" alt="ADA Logo" className="w-100 h-auto mx-auto" />
 
-    <Router>
-      <div className="min-h-screen bg-gray-100 p-8">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/industries" element={<Industries />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/careers" element={<Careers />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100 p-8">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/industries" element={<Industries />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
+    </div>
 
     
       
