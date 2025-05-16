491a6bc6367e28b7a202b190253778a376f7f3b7
zqalon <89801024+zqalon@users.noreply.github.com>
2025-05-16 13:43:29 -0700

    fixed spacing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import FadeIn from "../components/FadeIn";
 export default function Home() {
     // const navigate = useNavigate();
   return (
+    <FadeIn>
     <div className="text-center space-y-4">
 
       {/* <div className="flex items-center justify-center space-x-4">
@@ -26,28 +27,27 @@ export default function Home() {
           onClick={() => navigate('/careers')}
           label="Careers" />
       </div> */}
-      <FadeIn>
-      <div className="max-w-xl mx-auto p-6 bg-white rounded shadow">
-        <p className="text-left text-xl text-gray-800">ADA Consulting Incorporated is a business system consulting firm that specializes in the development and implementation of computerized business applications. We have successfully implemented technical solutions in the biotech, consumer products, medical products, wholesale and distribution industries.
-        We understand the business needs of the clients and industries we support whether you are a multi-national corporation or a small office with few employees, we have the expertise and experience that you need.</p>
-      </div>
-      <div className="max-w-xl mx-auto p-6 bg-white rounded shadow">
-        <h1 className="text-left text-xl text-gray-800">Consulting Services</h1>
-      </div>
-      <div className="flex items-center justify-center space-x-4">
-        <div className="max-w-sm p-6 bg-white rounded shadow">
-            <h2 className="text-left text-xl text-gray-800">JD Edwards Enterprise One</h2>
-            <p className="text-left text-m text-gray-800">Oracle's JD Edwards EnterpriseOne is a complete suite modular, pre-intergrated industry-specific business applications designed for rapid...
+        <div className="max-w-xl mx-auto p-6 bg-white rounded shadow">
+          <p className="text-left text-xl text-gray-800">ADA Consulting Incorporated is a business system consulting firm that specializes in the development and implementation of computerized business applications. We have successfully implemented technical solutions in the biotech, consumer products, medical products, wholesale and distribution industries.
+          We understand the business needs of the clients and industries we support whether you are a multi-national corporation or a small office with few employees, we have the expertise and experience that you need.</p>
+        </div>
+        <div className="max-w-xl mx-auto p-6 bg-white rounded shadow">
+          <h1 className="text-left text-xl text-gray-800">Consulting Services</h1>
+        </div>
+        <div className="flex items-center justify-center space-x-4">
+          <div className="max-w-sm p-6 bg-white rounded shadow">
+              <h2 className="text-left text-xl text-gray-800">JD Edwards Enterprise One</h2>
+              <p className="text-left text-m text-gray-800">Oracle's JD Edwards EnterpriseOne is a complete suite modular, pre-intergrated industry-specific business applications designed for rapid...
+              </p>
+          </div>
+          <div className="max-w-sm p-6 bg-white rounded shadow">
+            <h2 className="text-left text-xl text-gray-800">JD Edwards World</h2>
+            <p className="text-left text-m text-gray-800">Built for the IBM iSeries platform, Oracle's JD Edwards World offers a world-class enterprise solution for small businesses...
             </p>
+          </div>
         </div>
-        <div className="max-w-sm p-6 bg-white rounded shadow">
-          <h2 className="text-left text-xl text-gray-800">JD Edwards World</h2>
-          <p className="text-left text-m text-gray-800">Built for the IBM iSeries platform, Oracle's JD Edwards World offers a world-class enterprise solution for small businesses...
-          </p>
-        </div>
-      </div>
-      </FadeIn>
+
     </div>
-      
+    </FadeIn>
   );
 }
